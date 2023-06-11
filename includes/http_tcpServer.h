dca89a3ce3fe86da6a1c20b36f2398ed10a1efa5
Amanda Rodrigues Bravo <amandabravo@Amandas-MacBook-Pro.local>
2023-06-11 21:40:37 +0200

    start create server socket

--- includes/http_tcpServer.h
+++ includes/http_tcpServer.h
@@ -1,13 +1,51 @@
 #ifndef HTTP_TCPSERVER
 #define HTTP_TCPSERVER
 
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
 class TcpServer
 {
     public:
-        TcpServer();
+
+    // Constructor & Copy constructor
+        TcpServer( std::string ip_adress, int port );
+        TcpServer( const TcpServer& copy );
+    // Destructor
         ~TcpServer();
+    // Assignement Operator
+        TcpServer& operator=( const TcpServer& src );
+    
     private:
-        
+    // Functions
+        int     startServer( void );
+        void    closeServer( void );   
+    
+    // Member variable
+        std::string         _sIpAddress;
+        int                 _serverPort;
+        int                 _serverSocket;
+        int                 _serverNewSocket;
+        long                _serverIncomingMessage;
+    
+    // Server address
+        struct sockaddr_in  _serverAddress;
+        unsigned int        _socketAddressLen;
+        std::string         _serverMessage;
+    // serverAddress.sin_family = AF_INET;
+    // serverAddress.sin_port = htons(8080); // Port 8080
+    // serverAddress.sin_addr.s_addr = INADDR_ANY;
 };
+
+void log(const std::string &message){
+    std::cerr << message << std::endl;
+}
+void exitWithError(const std::string &errorMessage){
+    log("ERROR: " + errorMessage);
+    exit(1);
+}
 
 #endif
--- srcs/http_tcpServer.cpp
+++ srcs/http_tcpServer.cpp
@@ -1,5 +1,43 @@
 #include "http_tcpServer.h"
 
-TcpServer::TcpServer(){}
+// Constructor
+TcpServer::TcpServer( std::string ipAddress, int port ): _sIpAddress(ipAddress),
+_serverPort(port), _serverSocket(), _serverNewSocket(), _serverIncomingMessage(),
+_socketAddressLen(sizeof(_socketAddressLen)), _serverMessage(buildResponse()){
+    startServer();
+}
 
-TcpServer::~TcpServer(){}
+// Copy Constructor
+TcpServer::TcpServer( const TcpServer& copy ){
+    *this = copy;
+}
+
+// Destructor
+TcpServer::~TcpServer(){
+    closeServer();
+}
+
+// Assignement Operator
+TcpServer   &TcpServer::operator=( const TcpServer& src ){
+    if (this == &src)
+        return *this;
+    this->_serverSocket = src._serverSocket;
+    return *this;
+}
+
+// Functions
+int TcpServer::startServer(){
+    _serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (_serverSocket < 0)
+    {
+        exitWithError("Cannot create socket");
+        return 1;
+    }
+    return 0;
+}
+
+void TcpServer::closeServer(){
+    close(_serverSocket);
+    close(_serverNewSocket);
+    exit(0);
+}

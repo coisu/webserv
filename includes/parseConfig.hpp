36eb96b75b196d8886ffdb43efa05b80d29c6233
jischoi <60391224+coisu@users.noreply.github.com>
2023-06-12 19:24:19 +0000

    create: class for basic config

---
+++ includes/parseConfig.hpp
@@ -0,0 +1,14 @@
+# ifndef PARSECONFIG_HPP
+#define PARSECONFIG_HPP
+
+class PARSECONFIG_HPP
+{
+    private:
+        std::vector<serverConfig>   servers;
+        std::vector<std::string>    server_str;
+        int                         nbr_server;
+
+    public:     
+};
+
+#endif

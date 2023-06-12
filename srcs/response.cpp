e314ed2ec85e678a133e03ddee7ac5d49014bea5
jischoi <60391224+coisu@users.noreply.github.com>
2023-06-12 13:47:48 +0000

    added:  process priefly

--- srcs/response.cpp
+++ srcs/response.cpp
@@ -21,8 +21,39 @@
 // char *url;
 // char *proto;
 
-parse Request
--> set the env with parsed data
+
+-> read request (from fd 0 to max fd)
+-> parse Request & create vector(structure) for server as much as number of server
+
+	server can be more then one in config file.
+	probably, in the struct or class of Parser,
+	it has string array or vector<string> for the whole contents of each server config,
+	and also std::vector<configServer> and int(size_t) nb_server value
+	* based on the each string splited in vector<string>, set the each vector<configServer> value
+
+	And class configServer will has index, port, host, server_name, server_addr(sockaddr_in), max_body_client, listen_fd, maybe map for error and etc
+									+ struct or vector<string> for Location
+
+-> check is there duplicate server in the vector<configServer>
+-> create socket with server_address and server setting, (socket, setsockopt, bind)
+-> main loop start!!
+	> select 
+	goes through all file descriptors from 0 till the biggest fd in the set.
+
+	check file descriptors returend from select():
+ 	* if server fd --> accept new client
+ 	* if client fd in read_set --> read message from client
+ 	* if client fd in write_set:
+ 	* 1- If it's a CGI response and Body still not sent to CGI child process
+		--> Send request body to CGI child process.
+ 	* 2- If it's a CGI response and Body was sent to CGI child process
+		--> Read outupt from CGI child process.
+ 	* 3- If it's a normal response
+		--> Send response to client.
+
+-> accept connections,
+-> 
+-> parsing done successfully, check is the host name, port and servername
 -> pipe & fork before running CGI(need to be excute in child process)
 -> excute CGI with the env
 -> parse the result of CGI again
@@ -50,3 +81,30 @@ DELETE : delete resource that pointed by url
          *unsafety Method
 
 
+std::string 
+
+
+// headers.push_back(getDateHeader());
+// headers.push_back(getServerHeader(this));
+
+// if (status == CGI_SUCCESS_CODE)
+// 	createCGIResponse(status, headers, body);
+// if (status >= 400 && status <= 599)
+// {
+// 	if (!location.get_m_error_page().empty())
+// 		body = location.get_m_error_page();
+// 	else
+// 		body = m_default_error_page;
+// }
+// if (!ft::hasKey(ft::stringVectorToMap(headers), "Transfer-Encoding"))
+// 	headers.push_back("Content-Length:" + ft::to_string(body.size()));
+// if (!body.empty())
+// 		headers.push_back("Content-Language:ko-KR");
+// if (status / 100 != 2)
+// 	headers.push_back("Connection:close");
+// if (status / 100 == 3 && !ft::hasKey(ft::stringVectorToMap(headers), "Location"))
+// 	headers.push_back("Location:/");
+// if (status == 504 && !ft::hasKey(ft::stringVectorToMap(headers), "Retry-After"))
+// 	headers.push_back("Retry-After:3600");
+// if (connection.get_m_request().get_m_method() == Request::HEAD)
+// 	body.clear();

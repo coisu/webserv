b93c0ec7f013d393dc9a55ebb11239c79944af49
jischoi <60391224+coisu@users.noreply.github.com>
2023-06-15 12:42:38 +0000

    create: file for managing method before write response

---
+++ srcs/requestHandler.cpp
@@ -0,0 +1,10 @@
+#include "Response.hpp"
+
+// construct response class with request as arg > response.requestHandler(serverConfig),
+// response.buildResponse()
+
+Response::requestHandler(serverConfig &data)
+{
+    server = date;
+    if (request.getMethod)
+}

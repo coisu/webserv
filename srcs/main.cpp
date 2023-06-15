ffe63ffe85b58e589885f71c5d6a506882407afd
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-06-15 11:41:22 +0000

    renamed Method obj to Request in main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,7 +5,7 @@
 int main() {
 	// Create the socket
 	// parse_request(EXAMPLE_INPUT);
-	Method		get_method(EXAMPLE_INPUT);
+	Request		get_method(EXAMPLE_INPUT);
 	get_method.printHead();
 	TcpServer	server = TcpServer("0.0.0.0", 8081);
 

6ef2bc2e54767bda4f64a6a59b9309365a2947f2
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-06-18 20:15:27 +0000

    added random port generation to avoid problems

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,7 +5,8 @@
 Tconfig	temp_config = (Tconfig){"/workspaces/webserv", "cgi-bin"};
 
 int main(int ac, char **av) {
-	int	port = 8081;
+	srand((unsigned) time(NULL));
+	int port = 8080 + (rand() % 1000);
 	if (ac > 1)
 		port = atoi(av[1]);
 	TcpServer	server = TcpServer("0.0.0.0", port);

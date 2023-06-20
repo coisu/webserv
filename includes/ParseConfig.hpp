b2c0a569b04330bca4b35fb5a58f94ecc0a8493f
abravo31 <102750700+abravo31@users.noreply.github.com>
2023-06-20 17:02:47 +0000

    getters and setters done, miss to connect the server part with the config parsing

--- includes/ParseConfig.hpp
+++ includes/ParseConfig.hpp
@@ -26,6 +26,9 @@ enum tokenType
 	INIT
 };
 
+#define ERROR		-1
+#define SUCCESS		0
+
 typedef std::list < std::list < std::map < std::string, std::string > > > t_double_list;
 typedef std::list < std::map < std::string, std::string > > t_single_list;
 
@@ -42,42 +45,70 @@ class ParseConfig
         
         // Assignement Operator
 	        void    operator=( ParseConfig &other );
-
-        // Setters
-            void    setDefaultConfig( void );
         
         // Getters
             int                                 getStartProcess( void );
             void                                getFile( void );
-            t_double_list                       getList( void );
-            t_double_list                       getListInterface( void );
-            std::map<std::string, std:string>   getGlobalConfig( void );
-            std::map<std::string, std:string>   getGlobalConfigInterface( void );
-            std::vector<int>                    getPorts( void );
-            std::vector<int>                    getPortsInterface( void );
-            std::map<std::string, std::string>   getDefaultConfig( void );
-            std::map<std::string, std::string>   getDefaultConfigInterface( void );
+            t_double_list                       &getListInterface( void );
+            std::map<std::string, std::string>  &getGlobalConfigInterface( void );
+            std::vector<int>                    &getPortsInterface( void );
+            std::map<std::string, std::string>  &getDefaultConfigInterface( void );
+            tokenType                           getPrevToken( void );
+            std::string                         getDirectiveName( void );
+            std::string                         getDirectiveValue( void );
+            std::map<std::string, std::string>  getNewServer( void );
+            std::map<std::string, std::string>  getNewLocation( void );
+            int                                 getNewBracket( void );
+            std::string                         getWord( void );
+            std::string                         getConfigFile( void );
+            size_t                              getIndexConfig( void );
+            t_single_list                       getSingleList( void );
+            bool                                getIsLocation( void );
+            std::map<std::string, std::string>  getDefaultNewServer( void );
+            std::string                         getDirectory( void );
+
+        // Setters
+            void                                setDefaultConfig( void );
+            void                                setPrevToken( tokenType tokenToSet );
+            void                                setDirectiveName( std::string name );
+            void                                setDirectiveValue( std::string value );
+            void                                setNewServer( std::string name, std::string value );
+            void                                setNewServer( std::map<std::string, std::string> newServer );
+            void                                setNewLocation( std::string name, std::string value );
+            void                                setNewLocation( std::map<std::string, std::string> newLocation );
+            void                                setNewBracket( int value );
+            void                                setWord( std::string word );
+            void                                setSigleList( t_single_list sList );
+            void                                setIsLocation( bool act );
 
         // Methods
-            void    handleComments( std::string &line );
-            size_t  numberOfServerInterface( void );
-            size_t  numberOfServer( void );
-            bool    isNumber(std::string &str);
-            void    checkPort(std::string &strPort);
+            void        handleComments( std::string &line );
+            size_t      numberOfServerInterface( void );
+            bool        isNumber(std::string &str);
+            void        checkPort(std::string &strPort);
             std::string createToken(size_t &i);
 
         // Members variables
             std::string                             _fileName;
             bool                                    _errorHappened;
+            std::string                             _configFile;
             std::map<std::string, std::string>		_defautConfig;
             std::string                             _pwd;
             int                                     _bracketCounter;
             tokenType                               _previousToken;
             size_t                                  _indexConfigFile;
-            bool                                    _location;
+            bool                                    _isLocation;
+            std::map<std::string, std::string>		_newServer;
+            std::map<std::string, std::string>		_newLocation;
             t_double_list                           _serverList;
             std::map<std::string, std::string>      _globalConfig;
             std::vector<int>                        _ports;
+            std::string                             _directiveName;
+            std::string                             _directiveValue;
+            std::string                             _curWord;
+            t_single_list                           _sigleList;
+            std::string                             _pwd;
+
         //  unsigned long                           _clientMaxBodySize;
         // std::vector<serverConfig>   servers;
         // std::vector<std::string>    server_str;
@@ -85,8 +116,17 @@ class ParseConfig
 
     public:   
 
-    static	ParseConfig &getInstance(std::string fileName);
-	static	ParseConfig &getInstance();  
+    // Destructor
+        ~ParseConfig();
+
+    static  size_t                              numberOfServer( void );
+    bool                                        getErrorHappened( void );
+    static  std::map<std::string, std::string>  &getDefaultConfig( void );
+    static  std::map<std::string, std::string>  &getGlobalConfig( void );
+    static  ParseConfig                         &getInstance(std::string fileName);
+	static  ParseConfig                         &getInstance();
+    static  t_double_list                       &getList( void );
+    static  std::vector<int>                    &getPorts( void ); 
 };
 
 #endif
--- srcs/ParseConfig.cpp
+++ srcs/ParseConfig.cpp
@@ -4,11 +4,11 @@
 // this construct it called when there is no one given parameter to the program
 ParseConfig::ParseConfig(): _fileName("./resources/default.conf"), _configFile(std::string()), _errorHappened(false){
 
-    std::cout << "-----------\tRun with default file\t----------"
+    std::cout << "-----------\tRun with default file\t----------"<< std::endl;
     setDefaultConfig();
     int res = getStartProcess();
     if (res == ERROR)
-        _errorHappened(true);
+        _errorHappened = true;
 }
 
 ParseConfig & ParseConfig::	getInstance(std::string fileName)
@@ -37,9 +37,118 @@ ParseConfig::ParseConfig ( const ParseConfig &copy ){
 }
         
 // Assignement Operator
-void operator=( ParseConfig &other ){
+void ParseConfig::operator=( ParseConfig &other ){
     (void)other;
 }
+
+// Destructor
+ParseConfig::~ParseConfig(){}
+
+// ----------------------------- Getters -------------------------------------
+
+int     ParseConfig::getStartProcess( void ){
+    
+    try{
+        getFile();
+		_bracketCounter = 0;
+		_previousToken = INIT;
+		_indexConfigFile = 0;
+		_isLocation = false;
+		setDefaultConfig();
+		_newServer = _defautConfig;
+		//parse();
+		std::cout << ">> Config File [ OK ]" << std::endl;
+	} catch(char const * errorMsg){
+		std::cerr << errorMsg << std::endl;
+		return (ERROR);
+	}
+	return (SUCCESS);
+}
+
+/* Get the File object
+ * get file and store it in std::string _configFile
+ * get file line by line and then concated each line got previously
+ * contain the line of file */
+
+void	ParseConfig:: getFile()
+{
+	std::string line;
+	std::ifstream myReadFile(_fileName.c_str());
+	if (myReadFile.is_open())
+	{
+		while (getline (myReadFile, line)) 
+		{
+			_configFile.append(line);
+			handleComments(_configFile);
+			_configFile.append("\n");
+		}
+		myReadFile.close();
+	}
+	else
+		throw("error open file");
+}
+
+t_double_list &ParseConfig::getListInterface(){
+	return(_serverList);
+}
+
+t_double_list &ParseConfig::getList(){
+	return(getInstance(severFileName).getListInterface());
+}
+
+std::map<std::string, std::string>	&ParseConfig::getGlobalConfig(){
+	return(getInstance(severFileName).getGlobalConfigInterface());
+}
+
+std::map<std::string, std::string>   &ParseConfig::getGlobalConfigInterface(){
+	return(_globalConfig);
+}
+
+std::vector<int>	&ParseConfig::getPortsInterface(){
+	return(_ports);
+}
+
+std::vector<int>	&ParseConfig::getPorts(){
+	return(getInstance(severFileName).getPortsInterface());
+}
+
+std::map<std::string, std::string>	&ParseConfig::getDefaultConfigInterface(){
+	return(_defautConfig);
+}
+
+std::map<std::string, std::string>   &ParseConfig::getDefaultConfig(){
+	return(getInstance(severFileName).getDefaultConfigInterface());
+}
+
+bool	ParseConfig::getErrorHappened(){ return (_errorHappened); }
+
+tokenType	ParseConfig::getPrevToken(){ return(_previousToken); }
+
+std::string	ParseConfig::getDirectiveName(){ return(_directiveName); }
+
+std::string	ParseConfig::getDirectiveValue(){ return(_directiveValue); }
+
+std::map<std::string, std::string>	ParseConfig::getNewServer(){ return(_newServer); }
+
+std::map<std::string, std::string>	ParseConfig::getNewLocation(){ return(_newLocation); }
+
+int	ParseConfig::getNewBracket(){ return(_bracketCounter); }
+
+std::string	ParseConfig::getWord(){ return(_curWord); }
+
+std::string	ParseConfig::getConfigFile(){ return(_configFile); }
+
+size_t	ParseConfig::getIndexConfig(){ return(_indexConfigFile); }
+
+t_single_list	ParseConfig::getSingleList(){ return(_sigleList); }
+
+bool	ParseConfig::getIsLocation(){ return(_isLocation); }
+
+std::map<std::string, std::string>	ParseConfig::getDefaultNewServer(){ return(_defautConfig); }
+
+std::string	ParseConfig::getDirectory(){ return(_pwd); }
+
+//---------------------------- Setters -----------------------------------
 
 void	ParseConfig::setDefaultConfig( void ){
 	
@@ -61,86 +170,30 @@ void	ParseConfig::setDefaultConfig( void ){
 	}
 }
 
-// Getters
+void    ParseConfig::setPrevToken( tokenType tokenToSet ){ _previousToken = tokenToSet; }
 
-int     ParseConfig::getStartProcess( void ){
-    
-    try{
-        getFile();
-		_bracketCounter = 0;
-		_previousToken = INIT;
-		_indexConfigFile = 0;
-		_location = false;
-		setDefaultConfig();
-		_blockServer = _defautConfig;
-		//parse();
-		std::cout << ">> Config File [ OK ]" << std::endl;
-	} catch(char const * errorMsg){
-		std::cerr << errorMsg << std::endl;
-		return (ERROR);
-	}
-	return (SUCCESS);
-}
+void    ParseConfig::setDirectiveName( std::string name ){ _directiveName = name; }
+
+void    ParseConfig::setDirectiveValue( std::string value ){ _directiveValue = value; }
+
+void    ParseConfig::setNewServer( std::string name, std::string value ){ _newServer[name] = value; }
+
+void    ParseConfig::setNewServer( std::map<std::string, std::string> newServer ){ _newServer = newServer; }
+
+void    ParseConfig::setNewLocation( std::string name, std::string value ){ _newServer[name] = value; }
+
+void    ParseConfig::setNewLocation( std::map<std::string, std::string> newLocation ){ _newLocation = newLocation; }
+
+void    ParseConfig::setNewBracket( int value ){ _bracketCounter = value; }
+
+void    ParseConfig::setWord( std::string word ){ _curWord = word; }
+
+void    ParseConfig::setSigleList( t_single_list sList ){ _sigleList = sList; }
+
+void    ParseConfig::setIsLocation( bool act ){ _isLocation = act; }
 
 
-/* Get the File object
- * get file and store it in std::string _configFile
- * get file line by line and then concated each line got previously
- * contain the line of file */
-
-void	ParseConfig:: getFile()
-{
-	std::string line;
-	std::ifstream myReadFile(_fileName.c_str());
-	if (myReadFile.is_open())
-	{
-		while (getline (myReadFile, line)) 
-		{
-			_configFile.append(line);
-			handleCommentes(_configFile);
-			_configFile.append("\n");
-		}
-		myReadFile.close();
-	}
-	else
-		throw("error open file");
-}
-
-t_double_list &ParseConfig::getListInterface(){
-	return(_serverList);
-}
-
-t_double_list &ParseConfig::getList(){
-	return(getInstance(severFileName).getListInterface());
-}
-
-std::map<std::string, std:string>	&ParseConfig::getGlobalConfig(){
-	return(getInstance(severFileName).getGlobalConfigInterface());
-}
-
-std::map<std::string, std:string>   &ParseConfig::getGlobalConfigInterface(){
-	return(_globalConfig);
-}
-
-std::vector<int>	&ParseConfig::getPortsInterface(){
-	return(_ports);
-}
-
-std::vector<int>	&ParseConfig::getPorts(){
-	return(getInstance(severFileName).getPortsInterface());
-}
-
-std::map<std::string, std:string>	&ParseConfig::getDefaultConfigInterface(){
-	return(_defautConfig);
-}
-
-std::map<std::string, std:string>   &ParseConfig::getDefaultConfig(){
-	return(getInstance(severFileName).getDefaultConfigInterface());
-}
-
-bool	Parse_config_file::	getErrorHappened(){return (_errorHappened);}
-
-// Methods
+// --------------------------- Methods -----------------------------------
 
 /* This function remove comments line in give string 
  * comment line start with "#"
@@ -154,7 +207,7 @@ void	ParseConfig::handleComments(std::string &line)
 }
 
 size_t	ParseConfig::numberOfServer( void ){
-	return(getInstance(severFileName).NumberOfServerInterface());
+	return(getInstance(severFileName).numberOfServerInterface());
 }
 
 size_t	ParseConfig::numberOfServerInterface( void ){
@@ -165,6 +218,7 @@ size_t	ParseConfig::numberOfServerInterface( void ){
  * param str the given string to parse each character if is a digit character
  * return true  if given string in parameter is a number
  * return false if given string in parameter is not a number */
+
 bool	ParseConfig::isNumber(std::string &str)
 {
 	int res;
@@ -190,34 +244,65 @@ void	ParseConfig::checkPort(std::string &strPort)
 	_ports.push_back(intPort);
 }
 
-// /**
-//  * @brief Get the Piece Of string it is a token it return a token on each call
-//  * the goal is to get a word from string this word will be stored in @pieceOfString
-//  * this word came from '_configFile'
-//  * @_configFile is the config file
-//  * @start is the begin of word 
-//  * @nbCharacterTocopy number of character to copy from @start
-//  * @param i is index of string _configFile 
-//  * @return std::string 
-//  */
-// std::string	Parse_config_file::								create_token(size_t &i)
-// {
-// 	size_t	nbCharacterTocopy = 0;
-// 	size_t start = i;
+/* Get the Part Of string that is a token it return a token on each call
+ * the goal is to get a word from string, an than this word will be stored in @partOfStr
+ * this word came from '_configFile'
+ * _configFile is the config file
+ * start is the begin of word 
+ * toCopy number of character to copy from start
+ * param 'i' is index of string _configFile 
+ * return partOfStr */
+std::string	ParseConfig::createToken(size_t &i)
+{
+	size_t	toCopy = 0;
+	size_t	start = i;
 
-// 	if (_configFile[i] == '{' || _configFile[i] == '}' || _configFile[i] == ';')//if { or } or ; is the first character i going to catch that piece Of String
-// 	{
-// 		i++;
-// 		nbCharacterTocopy++;
-// 	}
-// 	else
-// 	{
-// 		while (!isspace(_configFile[i]) && (_configFile[i] != '{' && _configFile[i] != '}') && _configFile[i] != ';') // { or } or ; and whitespace character are delimiter
-// 		{
-// 			nbCharacterTocopy++;
-// 			i++;
-// 		}
-// 	}
-// 	std::string pieceOfString = _configFile.substr(start, nbCharacterTocopy);
-// 	return (pieceOfString);
-// }
+	if (_configFile[i] == '{' || _configFile[i] == '}' || _configFile[i] == ';')
+	{
+		i++;
+		toCopy++;
+	}
+	else
+	{
+		while (!isspace(_configFile[i]) && (_configFile[i] != '{' && _configFile[i] != '}') && _configFile[i] != ';')
+		{
+			toCopy++;
+			i++;
+		}
+	}
+	std::string partOfStr = _configFile.substr(start, toCopy);
+	return (partOfStr);
+}
+
+static bool	isKeyWord(std::string &word)
+{
+	if (word.compare("listen") == 0)
+		return (true);
+	if (word.compare("server_name") == 0)
+		return (true);
+	if (word.compare("root") == 0)
+		return (true);
+	if (word.compare("error_page") == 0)
+		return (true);
+	if (word.compare("autoindex") == 0)
+		return (true);
+	if (word.compare("client_body") == 0)
+		return (true);
+	if (word.compare("index") == 0)
+		return (true);
+	if (word.compare("methods") == 0)
+		return (true);
+	if (word.compare("allow") == 0)
+		return (true);
+	if (word.compare("cgi") == 0)
+		return (true);
+	if (word.compare("fastcgi_pass") == 0)
+		return (true);
+	if (word.compare("fastcgi_param") == 0)
+		return (true);
+	if (word.compare("return") == 0)
+		return (true);
+	if (word.compare("cgi_pass") == 0)
+		return (true);
+	return (false);
+}

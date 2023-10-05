5470101d3541a5512b1e1f696af456b98433e797
jisu <cscard1002@gmail.com>
2023-10-06 00:40:48 +0200

    fixed: added variable and setting functions

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -10,6 +10,13 @@
 #include <fstream>
 #include <unistd.h>
 #include "Request.hpp"
+#include "Mime.hpp"
+#include "Server.hpp"
+
+class Request;
+class Mime;
+class Server;
+
 
 class Response
 {
@@ -17,34 +24,44 @@ class Response
         std::string     _target_path;
         Location        _location;
         std::string     _body;
+        std::string     _buffer;
+        std::string     _headerStr;
         size_t          _body_len;
         bool            _auto_index;
         int             _status;
         bool            _req_status;
-        std::string     _checkCur;
+        std::string     _connect;
         std::map<std::string, std::string>     _headers;
+        Request         _request;
+        Server          _server;
 
     public:
         Mime            _mimeList;
-        Request         _request;
-        ServerConfig    _server;
-        
-
+        // Request         _request;
+        // Server          _server;
 
         Response();
-        Response( Request &request, const Server& server );
-        Response( int status, const Server& server );
+        // Response( Request &request, const Server& server );
+        // Response( int status, const Server& server );
         ~Response();
 
+
+    void    setServer(const Server &server);
+    void    setRequest(const Request &request);
+
+
     /* Constructor */
-    void		Response::initHeaders(void);
-    void		Response::initStatusCode(void);
+    void		initHeaders(void);
+    void		initStatusCode(void);
     /* Process */
-    void    Response::processResponse();
+    std::string    processResponse();
 
     /* Body Writer */
 
     /* Header Write */
+
+    void	clear();
+
 
 
 

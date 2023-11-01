9f9bf6edfba8684a830490484724fcb76b772a10
yoel <yridgway@student.42.fr>
2023-11-01 22:29:22 +0100

    added upload_store to env

--- srcs/CGI.cpp
+++ srcs/CGI.cpp
@@ -6,11 +6,6 @@ CGI::CGI(const Server& serv, const Location& location, const Request& request)
 	this->_env = constructEnv(request.getURL(), request.getMethodStr());
 	// std::cout << "CGI created\n";
 }
-// : server(serv), _cgiConfig(cgiConfig) // : _env(construct_env(request))
-// {
-// 	this->_env = constructEnv(RequestUrl, methodString);
-// 	// std::cout << "CGI created\n";
-// }
 
 CGI::~CGI()
 {
@@ -45,10 +40,11 @@ std::map<std::string, std::string>	CGI::constructEnv(std::string RequestUrl, std
 	env["SERVER_PORT"] = SSTR(this->server.getPort());
 	env["REQUEST_METHOD"] = methodString;
 	env["PATH_INFO"] = this->server.getRoot() + extractPathInfo(urlvec);
+	env["QUERY_STRING"] = extractQueryString(urlvec);
+	env["UPLOAD_STORE"] = this->_location.getUploadStore();
 	// if (!env["PATH_INFO"].empty())
 		// env["PATH_TRANSLATED"] = temp_config.root + env["PATH_INFO"];
 	// env["SCRIPT_NAME"] = temp_config.root + extractScriptName(urlvec);
-	env["QUERY_STRING"] = extractQueryString(urlvec);
 	// env["REMOTE_HOST"] = "";
 	// env["REMOTE_ADDR"] = "";
 	// env["AUTH_TYPE"] = "";
@@ -86,23 +82,8 @@ char**	CGI::getCharEnv( void )
 		strcpy(char_env[i], elem.c_str());
 	}
 	char_env[i] = NULL;
-	// std::cout << "\n\n-------ENV-------\n" << std::endl;
-	// i = 0;
-	// for (std::map<std::string, std::string>::iterator it = this->_env.begin(); it != this->_env.end(); it++)
-	// {
-	// 	std::cout << char_env[i] << std::endl;
-	// 	i++;
-	// }
-	// std::cout << "\n\n-----------------\n" << std::endl;
 	return (char_env);
 }
-
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <iostream>
-#include <string.h>
-#include <stdio.h>
 
 void CGI::exec_cgi( int &cgi_fd, int &cgi_pid)
 {
@@ -119,7 +100,6 @@ void CGI::exec_cgi( int &cgi_fd, int &cgi_pid)
 	if (pipe(pipefd) == -1)
 	{
 		perror("pipe"); // <-- COMMENT THIS OUT LATER
-		throw std::runtime_error("Failed to create pipe");
 		throw std::runtime_error("Failed to create pipe");
 	}
 
@@ -143,91 +123,23 @@ void CGI::exec_cgi( int &cgi_fd, int &cgi_pid)
 
 		// Execute the CGI program
 		if (execve(cgi_path, argv, envp) == -1)
-		{
 			perror("execve"); // <-- COMMENT THIS OUT LATER
-		}
-		// exit(1);
+		exit(1);
 	}
 	else
 	{
 		// Parent process
-		cgi_pid = pid;
-		// Close write end in parent
-		close(pipefd[1]);
+
+		cgi_pid = pid; // <-- Save the CGI process ID
+		close(pipefd[1]); // <-- Close write end in parent
 	}
 	for (int i = 0; envp[i]; i++)
 		delete[] envp[i];
 	delete[] envp;
 }
 
-// std::string	CGI::exec_cgi( void )
-// {
-// 	const char*	cmd;
-// 	std::string	strcmd;
-// 	char		buffer[128];
-// 	std::string	envline;
-
-// 	for (std::map<std::string, std::string>::iterator it = this->_env.begin(); it != this->_env.end(); it++)
-// 		envline += (it->first + "=\'" + it->second + "\' ");
-// 	strcmd = "echo \"" + this->_env["QUERY_STRING"] + "\" hello |" + envline + this->_program + " " + this->_script;
-// 	cmd = strcmd.c_str();
-// 	// std::cout << strcmd << std::endl;
-// 	std::string result = "";
-// 	FILE* pipe = popen(cmd, "r");
-// 	if (!pipe)
-// 		throw std::runtime_error("popen() failed!");
-// 	try
-// 	{
-// 		while (fgets(buffer, sizeof buffer, pipe) != NULL)
-// 			result += buffer;
-// 	}
-// 	catch (...)
-// 	{
-// 		pclose(pipe);
-// 		throw ;
-// 	}
-// 	pclose(pipe);
-// 	return result;
-// }
-
-// std::string	CGI::exec_cgi( void )
-// {
-// 	// char	buffer[1024];
-// 	int	pipe_in[2];
-// 	int	pipe_out[2];
-// 	int	pid;
-// 	int	exit_status;
-
-// 	if (pipe(pipe_in) < 0)
-// 		std::cerr << "pipe() failed" << std::endl, throw 500;
-// 	if (pipe(pipe_out) < 0)
-// 	{
-// 		std::cerr << "pipe() failed" << std::endl;
-// 		close(pipe_in[0]), close(pipe_in[1]), throw 500;
-// 	}
-// 	pid = fork();
-// 	if (pid == 0)
-// 	{
-// 		dup2(pipe_in[0], STDIN_FILENO);
-// 		dup2(pipe_out[1], STDOUT_FILENO);
-// 		close(pipe_in[0]);
-// 		close(pipe_in[1]);
-// 		close(pipe_out[0]);
-// 		close(pipe_out[1]);
-// 		exit_status = execve(this->_av[0], this->_av, this->getCharEnv());
-// 		exit(exit_status);
-// 	}
-// 	else if (pid < 0)
-// 		std::cout << "Fork failed" << std::endl, throw 500;
-// 	return (res);
-// }
-
 void	CGI::identifyCGI(std::vector<std::string> urlvec)
 {
-	// std::cout << "URLVEC:\n";
-	// for (size_t i = 0; i < urlvec.size(); i++)
-	// 	std::cout << urlvec[i] << ", ";
-	// std::cout << "end\n";
 	std::string path, ext;
 	std::string root = this->server.getRoot();
 	for (size_t i = 0; i < urlvec.size() && urlvec[i][0] != '?'; i++)
@@ -245,23 +157,6 @@ void	CGI::identifyCGI(std::vector<std::string> urlvec)
 	this->_script = root + path;
 	this->_postfix = ext;
 	this->_program = this->_cgiConfig[this->_postfix];
-	// this->_program = this->_cgiConfig[this->_postfix];
-
-	// for (size_t i = 0; i < urlvec.size(); i++)
-	// {
-	// 	for (std::map<std::string, std::string>::iterator it = this->_cgiConfig.begin(); 
-	// 	it != this->_cgiConfig.end(); it++)
-	// 	{
-	// 		if (urlvec[i].find(it->first) == urlvec[i].size() - 3)
-	// 			this->_script = root + extractPathInfo(urlvec), this->_postfix = it->first;
-	// 			// this->_script = this->server.getRoot() + temp_config.cgi_folder + urlvec[i], this->_postfix = it->first;
-	// 	}
-	// }
-	// // _script = "/workspaces/webserv/cgi-bin/";
-	// // _script = "/workspaces/webserv/post/post.php";
-	// if (this->_script.empty())
-	// 	throw 501;
-	// this->_program = this->_cgiConfig[this->_postfix];
 }
 
 std::string	CGI::extractScriptName(std::vector<std::string> urlvec)
